b9d19a4d16328184dc9484c2bc5ba061ebc93710
Kylan <kylan@hey.com>
2024-11-03 01:32:12 -0800

    forgor to commit

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -65,8 +65,8 @@ public static class FunctionLibrary
 
     public static Vector3 Torus(float u, float v, float t)
     {
-        var r1 = .75f;
-        var r2 = .25f;
+        var r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        var r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
         var s = r1 + r2 * Cos(PI * v);
         Vector3 p;
         p.x = s * Sin(PI * u);
