33b713160daa92fe341dc238e54888a235527f12
Kylan <kylan@hey.com>
2024-11-04 00:49:46 -0800

    Added best and worst frame rates for sample period to display

--- Assets/Scripts/FrameRateCounter.cs
+++ Assets/Scripts/FrameRateCounter.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FrameRateCounter : MonoBehaviour
 {
-    [Seriali]
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI display;
+    [SerializeField] [Range(.1f, 2f)] private float SampleDuration = 1f;
+    private int frames = 0;
+    private float duration = 0, bestDuration = float.MaxValue, worstDuration;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var frameDuration = Time.unscaledDeltaTime;
+        frames++;
+        duration += frameDuration;
+        if (frameDuration < bestDuration) bestDuration = frameDuration;
+        if (frameDuration > worstDuration) worstDuration = frameDuration;
+        if (duration > SampleDuration)
+        {
+            display.SetText("FPS\n{0:0}\n{1:0}\n{2:0}", 1f / bestDuration, frames / duration, 1f / worstDuration);
+            frames = 0;
+            duration = 0f;
+            bestDuration = float.MaxValue;
+            worstDuration = 0f;
+        }
     }
 }
